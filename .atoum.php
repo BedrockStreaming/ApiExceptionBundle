9a7e29ac026dbcd640f77c92c51dabf21b0a46d2
jubianchi <contact@jubianchi.fr>
2017-02-25 10:20:46 +0100

    Update to newer atoum versions
    atoum 2.8 allows us to remove the bootstrap when it's only used to
    require the autoloader (see https://github.com/atoum/atoum/pull/605)
    
    atoum 3.0 allows us to run tests on latest PHP versions.

--- .atoum.php
+++ .atoum.php
@@ -1,4 +1,13 @@
 <?php
 
-$runner->setBootstrapFile(__DIR__ . '/Tests/bootstrap.php');
 $runner->addTestsFromDirectory(__DIR__.'/Tests');
+
+if (getenv('TRAVIS') !== false) {
+    $script->addDefaultReport();
+
+    $cloverWriter = new atoum\writers\file(__DIR__.'/clover.xml');
+    $cloverReport = new atoum\reports\asynchronous\clover();
+    $cloverReport->addWriter($cloverWriter);
+
+    $runner->addReport($cloverReport);
+}
